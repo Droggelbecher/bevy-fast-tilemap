96dae87c072c0a1ea1f554cacf267d4d274071ce
Henning Hasemann <118525+Droggelbecher@users.noreply.github.com>
2024-06-02 12:06:19 +0200

    wip

--- examples/pattern.rs
+++ examples/pattern.rs
@@ -5,7 +5,7 @@ use bevy::{
     render::render_resource::{AsBindGroup, ShaderType},
     window::PresentMode,
 };
-use bevy_fast_tilemap::{CustomFastTileMapPlugin, FastTileMapPlugin, Map, MapBundleManaged};
+use bevy_fast_tilemap::{CustomFastTileMapPlugin, Map, MapBundleManaged};
 use rand::Rng;
 
 #[path = "common/mouse_controls_camera.rs"]
@@ -40,35 +40,69 @@ fn main() {
                         x: u32,
                     };
 
-                    fn offs(x: f32) -> f32 {
-                        // make sure the frequency is a multiple of the tile size
-                        // so the corners of the tiles align
+                    fn offs_spikey(x: f32) -> f32 {
                         var pi = radians(180.0);
-                        return 1.0 * sin(x * pi / 8.0);
+                        return 1.0 * sin(x + cos(x));
+                    }
+
+                    fn offs_rocky(x: f32) -> f32 {
+                        var pi = radians(180.0);
+                        return 0.7 * (
+                              0.1 * sin(x / 3.0)
+                            + 0.1 * sin((x+0.1) / 3.2)
+                            + 0.1 * sin((x+0.2) / 2.8)
+                            + 0.2 * sin((x+1) / 1.0)
+                            + 0.1 * sin((x+2.1) / 0.5)
+                            + 0.1 * sin((x+2.2) / 0.4)
+                            + 0.1 * sin((x+2.3) / 0.3));
+                    }
+
+                    fn offs(x: f32) -> f32 {
+                        return 12.0 * offs_rocky(x / 6.0);
+                    }
+
+                    // DEBUG: Render tiles flat
+                    fn dbg_sample_tile(in: ExtractIn) -> vec4<f32> {
+                        return sample_tile_at(in.tile_index, in.tile_position, in.tile_offset);
                     }
 
                     fn sample_tile(in: ExtractIn) -> vec4<f32> {
-                        // DEBUG
-                        //if in.tile_offset.x <= 1.0 || in.tile_offset.y <= 1.0 {
+                        // offs() is shifted this much inwards
+                        // Bigger means smaller tiles and more fluid borders with eg diagonal
+                        // neighbors. Needs to be big enough so offs()+bw doesnt spill over into the
+                        // neighboring tile.
+                        var doffs = 8.0;
+
+                        // half the border width
+                        var bw = 2.0;
+
+                        // Color of the border
+                        //var border_color = vec4<f32>(0.0, 138.0/256.0, 170.0/256.0, 1.0);
+                        var border_color = vec4<f32>(0.0, 0.0, 0.0, 1.0);
+
+                        // How fast should the border move? (0.0 = no movement)
+                        var animation_speed = 0.0;
+
+                        // DEBUG: Render rectangular tile borders
+                        //if in.tile_offset.x <= 0.1 || in.tile_offset.y <= 0.1 {
                             //return vec4<f32>(1.0, 0.0, 0.0, 1.0);
                         //}
 
                         var tile_index = in.tile_index;
 
-                        // offs() is shifted this much inwards
-                        var doffs = 2.0;
+                        var a = in.animation_state * animation_speed;
+
+                        // shift between left/right and top/bottom
+                        var shift = 100.0;
 
                         // right boundary
-                        var r = map.tile_size.x + offs(f32(in.tile_position.y) * map.tile_size.y + in.tile_offset.y) - doffs;
+                        var r = map.tile_size.x + offs(f32(in.tile_position.y) * map.tile_size.y + in.tile_offset.y + a) - doffs;
                         // left boundary
-                        var l = offs(f32(in.tile_position.y) * map.tile_size.y + in.tile_offset.y) + doffs;
+                        var l = offs(f32(in.tile_position.y) * map.tile_size.y + in.tile_offset.y - a + shift) + doffs;
                         // top boundary
-                        var t = map.tile_size.y + offs(f32(in.tile_position.x) * map.tile_size.x + in.tile_offset.x) - doffs;
+                        var t = map.tile_size.y + offs(f32(in.tile_position.x) * map.tile_size.x + in.tile_offset.x - a) - doffs;
                         // bottom boundary
-                        var b = offs(f32(in.tile_position.x) * map.tile_size.x + in.tile_offset.x) + doffs;
-
-                        // border width
-                        var bw = 0.2;
+                        var b = offs(f32(in.tile_position.x) * map.tile_size.x + in.tile_offset.x + a + shift) + doffs;
 
                         // are we in a neighbor tile (or in a border to it)?
                         var tile_offset = vec2<i32>(0, 0);
@@ -88,7 +122,7 @@ fn main() {
                         }
                         // dominance rule: highest tile index wins
                         var tile_position = in.tile_position + tile_offset;
-                        tile_index = get_tile_index(tile_position);
+                        tile_index = get_tile_index_checked(tile_position);
                         var max_index = max(in.tile_index, tile_index);
 
                         // Figure out border drawing.
@@ -104,6 +138,15 @@ fn main() {
 
                         var is_xborder = abs(in.tile_offset.x - r) <= bw || abs(in.tile_offset.x - l) <= bw;
                         var is_yborder = abs(in.tile_offset.y - t) <= bw || abs(in.tile_offset.y - b) <= bw;
+
+                        // DEBUG
+                        //var d = 0.02;
+                        //if abs(in.tile_offset.x - r) <= d || abs(in.tile_offset.x - l) <= d {
+                            //return vec4<f32>(0.0, 1.0, 0.0, 1.0);
+                        //}
+                        //if abs(in.tile_offset.y - t) <= d || abs(in.tile_offset.y - b) <= d {
+                            //return vec4<f32>(0.0, 1.0, 1.0, 1.0);
+                        //}
 
                         // diagonal case
                         if tile_offset.x != 0 && tile_offset.y != 0 {
@@ -127,35 +170,33 @@ fn main() {
                             // the current tile (C) and the three neighbors (R, B, D).
                             // `max_index` at this point is already max(C, D), so we only need to
                             // include R & B.
-                            max_index = max(max_index, get_tile_index(in.tile_position + vec2<i32>(tile_offset.x, 0)));
-                            max_index = max(max_index, get_tile_index(in.tile_position + vec2<i32>(0, tile_offset.y)));
+                            max_index = max(max_index, get_tile_index_checked(in.tile_position + vec2<i32>(tile_offset.x, 0)));
+                            max_index = max(max_index, get_tile_index_checked(in.tile_position + vec2<i32>(0, tile_offset.y)));
 
-
-                            if is_xborder && max(in.tile_index, get_tile_index(in.tile_position + vec2<i32>(0, tile_offset.y))) != max_index {
+                            if is_xborder && max(in.tile_index, get_tile_index_checked(in.tile_position + vec2<i32>(0, tile_offset.y))) != max_index {
+                                // The "|" left of (d) in the picture above
                                 is_border = true;
-                                //return vec4<f32>(1.0, 0.0, 0.0, 1.0);
                             }
-                            if is_yborder && max(in.tile_index, get_tile_index(in.tile_position + vec2<i32>(tile_offset.x, 0))) != max_index {
+                            if is_yborder && max(in.tile_index, get_tile_index_checked(in.tile_position + vec2<i32>(tile_offset.x, 0))) != max_index {
+                                // The "-" above (d) in the picture above
                                 is_border = true;
-                                //return vec4<f32>(1.0, 0.0, 1.0, 1.0);
+                            }
+                            if is_xborder && is_yborder && max_index > in.tile_index {
+                                // The little "+" between (C) and (d) in the picture above
+                                is_border = true;
                             }
                         }
                         else if (is_xborder || is_yborder) && max_index > in.tile_index {
-                                is_border = true;
-                                //return vec4<f32>(0.0, 1.0, 0.0, 1.0);
+                            is_border = true;
                         }
 
                         if is_border
                         {
-                            return vec4<f32>(0.0, 0.0, 0.0, 1.0);
+                            return border_color;
                         }
 
                         var color = sample_tile_at(max_index, in.tile_position, in.tile_offset);
                         return color;
-                    }
-
-                    fn dominates_index(p: vec2<i32>, index: u32) -> bool {
-                        return get_tile_index(p) > index;
                     }
 
                     "#.to_string()
@@ -175,19 +216,19 @@ fn startup(
     commands.spawn(Camera2dBundle::default());
 
     let map = Map::<UserData>::builder(
-        uvec2(64, 64),
+        uvec2(16, 16),
         asset_server.load("patterns.png"),
-        vec2(16., 16.),
+        vec2(64., 64.),
     )
-    // A tile (pattern) in the atlas is 8x8 map tiles large
-    // (i.e. it will repeat every 8 tiles in any direction)
-    .with_atlas_tile_size_factor(8)
+    // A tile (pattern) in the atlas is NxN map tiles large
+    // (i.e. it will repeat every N tiles in any direction)
+    .with_atlas_tile_size_factor(4)
     .build_and_initialize(|m| {
         let mut rng = rand::thread_rng();
 
         for y in 0..m.size().y {
             for x in 0..m.size().y {
-                m.set(x, y, rng.gen_range(0..2));
+                m.set(x, y, rng.gen_range(0..4));
             }
         }
     });
