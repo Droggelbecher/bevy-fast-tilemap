bcf05e0f17b13dd93e64a93eeb8aad7b6b2bc482
Henning Hasemann <118525+Droggelbecher@users.noreply.github.com>
2024-06-02 12:28:23 +0200

    wip

--- examples/pattern.rs
+++ examples/pattern.rs
@@ -40,11 +40,7 @@ fn main() {
                         x: u32,
                     };
 
-                    fn offs_spikey(x: f32) -> f32 {
-                        var pi = radians(180.0);
-                        return 1.0 * sin(x + cos(x));
-                    }
-
+                    // Simple 1d function that defines the irregular boundary shape
                     // Returns values in the range -1.0 to 1.0
                     fn offs_rocky(x: f32) -> f32 {
                         var pi = radians(180.0);
@@ -58,6 +54,7 @@ fn main() {
                             + 0.1 * sin((x+2.3) / 0.3));
                     }
 
+                    // Scaled version.
                     // Returns values in the range -6.0 to 6.0
                     fn offs(x: f32) -> f32 {
                         return 6.0 * offs_rocky(x / 6.0);
@@ -80,38 +77,36 @@ fn main() {
                         // half the border width
                         var bw = 2.0;
 
+                        // debug borders
+                        var bw_dbg = -1.0;
+
                         // Color of the border
-                        //var border_color = vec4<f32>(0.0, 138.0/256.0, 170.0/256.0, 1.0);
                         var border_color = vec4<f32>(0.0, 0.0, 0.0, 1.0);
 
                         // How fast should the border move? (0.0 = no movement)
+                        // This makes for a more psychedelic effect and can be pretty distracting.
                         var animation_speed = 0.0;
 
                         // DEBUG: Render rectangular tile borders
-                        //if in.tile_offset.x <= 0.1 || in.tile_offset.y <= 0.1 {
-                            //return vec4<f32>(1.0, 0.0, 0.0, 1.0);
-                        //}
+                        if in.tile_offset.x <= 2.0 * bw_dbg || in.tile_offset.y <= 2.0 * bw_dbg {
+                            return vec4<f32>(1.0, 0.0, 0.0, 1.0);
+                        }
 
                         var tile_index = in.tile_index;
-
                         var a = in.animation_state * animation_speed;
 
-                        // shift between left/right and top/bottom
+                        // shift between left/right and top/bottom offs() calls.
+                        // This way opposing sides of a tile don't look exactly the same.
                         var shift = 100.0;
 
-                        // right boundary
+                        // Positions of the (inner) irregular boundaries inside this tile
                         var r = map.tile_size.x + offs(f32(in.tile_position.y) * map.tile_size.y + in.tile_offset.y + a) - doffs;
-                        // left boundary
                         var l = offs(f32(in.tile_position.y) * map.tile_size.y + in.tile_offset.y - a + shift) + doffs;
-                        // top boundary
                         var t = map.tile_size.y + offs(f32(in.tile_position.x) * map.tile_size.x + in.tile_offset.x - a) - doffs;
-                        // bottom boundary
                         var b = offs(f32(in.tile_position.x) * map.tile_size.x + in.tile_offset.x + a + shift) + doffs;
 
-                        // are we in a neighbor tile (or in a border to it)?
-                        var tile_offset = vec2<i32>(0, 0);
-
                         // determine actual tile index to render at this position
+                        var tile_offset = vec2<i32>(0, 0);
                         if in.tile_offset.x > r - bw {
                             tile_offset = tile_offset + vec2<i32>(1, 0);
                         }
@@ -124,33 +119,36 @@ fn main() {
                         else if in.tile_offset.y <= b + bw {
                             tile_offset = tile_offset + vec2<i32>(0, -1);
                         }
-                        // dominance rule: highest tile index wins
                         var tile_position = in.tile_position + tile_offset;
                         tile_index = get_tile_index_checked(tile_position);
+
+                        // Dominance rule: highest tile index wins.
+                        // I.e. high tile indices will form diagonal connections at the expense of
+                        // lower indices.
                         var max_index = max(in.tile_index, tile_index);
 
                         // Figure out border drawing.
-                        // Borders are drawn only between tiles of different indices.
+                        // Borders are drawn only between tiles of *different indices*.
                         // In the easy case the border is between "this" tile index (in.tile_index)
                         // and some horizontal/vertical neighbor.
                         //
                         // It might however also be between a horizontal/vertical neighbor and a
-                        // diagonal neighbor. In that case we need to compare our neighbors indices
-                        // with each other
+                        // diagonal neighbor, in which case it has nothing to do with in.tile_index.
+                        // In that case we need to compare our neighbors indices with each other.
 
                         var is_border = false;
 
                         var is_xborder = abs(in.tile_offset.x - r) <= bw || abs(in.tile_offset.x - l) <= bw;
                         var is_yborder = abs(in.tile_offset.y - t) <= bw || abs(in.tile_offset.y - b) <= bw;
 
-                        // DEBUG
-                        //var d = 0.02;
-                        //if abs(in.tile_offset.x - r) <= d || abs(in.tile_offset.x - l) <= d {
-                            //return vec4<f32>(0.0, 1.0, 0.0, 1.0);
-                        //}
-                        //if abs(in.tile_offset.y - t) <= d || abs(in.tile_offset.y - b) <= d {
-                            //return vec4<f32>(0.0, 1.0, 1.0, 1.0);
-                        //}
+                        // DEBUG: Render offs() instances
+                        var d = 0.02;
+                        if abs(in.tile_offset.x - r) <= bw_dbg || abs(in.tile_offset.x - l) <= bw_dbg {
+                            return vec4<f32>(0.0, 1.0, 0.0, 1.0);
+                        }
+                        if abs(in.tile_offset.y - t) <= bw_dbg || abs(in.tile_offset.y - b) <= bw_dbg {
+                            return vec4<f32>(0.0, 1.0, 1.0, 1.0);
+                        }
 
                         // diagonal case
                         if tile_offset.x != 0 && tile_offset.y != 0 {
