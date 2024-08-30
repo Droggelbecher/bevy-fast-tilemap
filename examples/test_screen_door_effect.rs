1eb9620f5ef4f684198597044b9acbd3fc060772
Henning Hasemann <118525+Droggelbecher@users.noreply.github.com>
2024-08-30 20:47:23 +0200

    Combo of atlas resizing & single-triangle mesh seems to work?

--- examples/test_screen_door_effect.rs
+++ examples/test_screen_door_effect.rs
@@ -31,6 +31,7 @@ fn main() {
             WorldInspectorPlugin::new()
         ))
         .add_systems(Startup, startup)
+        .add_systems(Update, highlight_hovered)
         .run();
 }
 
@@ -51,9 +52,16 @@ fn startup(
 
     let map = Map::builder(
         uvec2(640, 640),
-        asset_server.load("debug_32x32_pad_1x1.png"),
+        // This still gives occasional screen door effect, depending on the zoom level
+        // asset_server.load("debug_32x32_pad_1x1.png"),
+
+        // This seems to work now
+        asset_server.load("debug_32x32_pad_1x1_resized.png"),
         vec2(32., 32.),
     )
+
+    // This is necessary for the "resized" version to be usable
+    .with_n_tiles(Some(uvec2(8, 6)))
     .with_padding(vec2(1.0, 1.0), vec2(1.0, 1.0), vec2(1.0, 1.0))
     .build_and_initialize(|m| {
         // Initialize using a closure
@@ -72,3 +80,48 @@ fn startup(
     ;
 
 }
+
+/// Highlight the currently hovered tile red, reset all other tiles
+fn highlight_hovered(
+    mut cursor_moved_events: EventReader<CursorMoved>,
+    mut camera_query: Query<(&GlobalTransform, &Camera), With<OrthographicProjection>>,
+    maps: Query<&Handle<Map>>,
+
+    // We'll actually change the map contents for highlighting
+    mut materials: ResMut<Assets<Map>>,
+) {
+    for event in cursor_moved_events.read() {
+        for map_handle in maps.iter() {
+            let map = materials.get_mut(map_handle).unwrap();
+
+            for (global, camera) in camera_query.iter_mut() {
+                // Translate viewport coordinates to world coordinates
+                if let Some(world) = camera
+                    .viewport_to_world(global, event.position)
+                    .map(|ray| ray.origin.truncate())
+                {
+                    // The map can convert between world coordinates and map coordinates for us
+                    let coord = map.world_to_map(world);
+                    println!("Map coordinate: {:?}", coord);
+
+                    // let coord = coord
+                    //     .as_uvec2()
+                    //     .clamp(uvec2(0, 0), map.map_size() - uvec2(1, 1));
+
+                    // Modifying the map requires that the underlying data be synchronized to
+                    // the GPU again so you want to avoid to do this every frame if your map is
+                    // very large. The transfer cost does not depend on how much you change, so
+                    // you may as well generate the whole thing (of course consider the actual
+                    // generation time).
+                    //
+                    // Note that this technically does *not* modify the `Map` component, but
+                    // the underlying data which is stored in the material.
+                    // let mut m = map.indexer_mut();
+
+                    // reset_map(&mut m);
+                    // m.set_uvec(coord, 3u32);
+                } // if Some(world)
+            } // for (global, camera)
+        } // for map
+    } // for event
+} // highlight_hovered
